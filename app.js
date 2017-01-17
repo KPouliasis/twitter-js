69a0fc7e802785a91343d52ef88c50b66775e070
Jessica Perina <jperina@gmail.com>
2017-01-17 10:26:57 -0500

    reordered routes

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 'use strict';
 
+const volleyball = require('volleyball');
 const chalk = require('chalk');
+const http = require('http');
 const express = require('express');
 
 const app = express();
 
-app.listen(3000, function() {
-	console.log('server listening');
-});
+// app.use(volleyball);
 
 app.use(function(req, res, next) {
-	console.log(chalk.blue(req.path.slice(1)));
+	// console.log(chalk.blue(req.path.slice(1)));
+	let type = req.method;
+	let code = res.statusCode;
+	let status = http.STATUS_CODES[code];
+	let path = req.url;
+	console.log(type, path, code, status);
 	next();
 })
 
 app.use('/special/', function(req, res, next) {
-		console.log(chalk.red('the secret zone'));
+	console.log(chalk.red('the secret zone'));
+	next();
 })
 
 app.get('/', function(req, res, next) {
@@ -25,3 +31,7 @@ app.get('/', function(req, res, next) {
 app.get('/news', function(req, res, next) {
 	res.send("All the news that's fit to print");
 })
+
+app.listen(3000, function() {
+	console.log('server listening');
+});
