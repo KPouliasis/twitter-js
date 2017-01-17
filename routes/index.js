9d830b4435eb3deecb9c434f3b35e996cb4bba00
existentialtype <konstantinos.pouliasis@gmail.com>
2017-01-17 13:00:05 -0500

    twitter options

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+router.use(express.static('public'))
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
