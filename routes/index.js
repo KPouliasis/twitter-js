2fd592aae4ea2295140fad4ea3e81117fa8e5e2c
existentialtype <konstantinos.pouliasis@gmail.com>
2017-01-17 15:46:49 -0500

    sockets in, emmiting kinda

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,3 @@
-const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser')
-// could use one line instead: const router = require('express').Router();
-const tweetBank = require('../tweetBank');
-
-router.use(express.static('public'))
-router.use(bodyParser.urlencoded({ extended: false }))
-router.use(bodyParser.json())
-
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets, showForm : true } );
-});
-
-// parse application/x-www-form-urlencoded
-
-
-// router.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted:\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-// })
-
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets : list, showForm : true, name : name } );
-});
-
-router.get('/tweets/:id', function(req,res){
-  var id = req.params.id;
-  var tweet = tweetBank.find({id:id})
-  res.render( 'index', { tweets : tweet, showForm : false } );
-})
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-// POST /login gets urlencoded bodies
-router.post('/tweets', function(req, res) {
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('/');
-});
 
 
 module.exports = function(io){
@@ -60,15 +15,6 @@ module.exports = function(io){
     let tweets = tweetBank.list();
     res.render( 'index', { tweets: tweets, showForm : true } );
   });
-
-  // parse application/x-www-form-urlencoded
-
-
-  // router.use(function (req, res) {
-  //   res.setHeader('Content-Type', 'text/plain')
-  //   res.write('you posted:\n')
-  //   res.end(JSON.stringify(req.body, null, 2))
-  // })
 
   router.get('/users/:name', function(req, res) {
     var name = req.params.name;
@@ -88,7 +34,9 @@ module.exports = function(io){
   router.post('/tweets', function(req, res) {
     var name = req.body.name;
     var text = req.body.text;
+    var tweet = "mooo"
     tweetBank.add(name, text);
+    io.sockets.emit('newTweet',tweet)
     res.redirect('/');
   });
 
